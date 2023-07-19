d26ad9741ddf4b67817f4d9be372e94470a1dbb0
Sapronov Konstantin <ks.costas@gmail.com>
2023-07-19 16:09:39 +0300

    refactor: move provider to main

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,15 +2,10 @@ import { ReactElement } from 'react';
 
 import './App.css';
 
-import { CounterProvider } from '../store';
 import { HomePage } from '../pages';
 
 const App = (): ReactElement => {
-  return (
-    <CounterProvider>
-      <HomePage />
-    </CounterProvider>
-  );
+  return <HomePage />;
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './app/App.tsx';
+import { CounterProvider } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <CounterProvider>
+      <App />
+    </CounterProvider>
   </React.StrictMode>
 );
