cec3467379df4ebbbc8454ed089476b0c7c184e4
Sapronov Konstantin <ks.costas@gmail.com>
2023-07-19 16:11:36 +0300

    refactor: remove page component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,15 @@ import { ReactElement } from 'react';
 
 import './App.css';
 
-import { HomePage } from '../pages';
+import { CounterValue } from '../components/counter-value/CounterValue.tsx';
+import { CounterForm } from '../components/counter-form/CounterForm.tsx';
 
-const App = (): ReactElement => {
-  return <HomePage />;
-};
+const App = (): ReactElement => (
+  <div className="container mx-auto px-4 pt-5 flex flex-col items-center">
+    <h1 className="text-3xl font-bold">Counter</h1>
+    <CounterValue />
+    <CounterForm />
+  </div>
+);
 
 export default App;
